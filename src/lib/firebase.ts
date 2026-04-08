b205c3007baa2f3b575bdaf7ebe542a71826c0b8
OUmrani yacine <oumraniyacine1@gmail.com>
2026-04-08 15:48:06 +0100

    updete

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  firestoreDatabaseId: process.env.VITE_FIREBASE_DATABASE_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
